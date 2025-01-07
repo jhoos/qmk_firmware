a8bb008de7b57ee0e5712ab5cc0a2a1d71841e7e
Jason Hoos <jhoos@thwack.net>
2025-01-06 19:29:32 -0600

    First GMMK2 colorization/layout
    Basic color coding of keys by type, with color for activation of caps/caps word/num lock,
    and keypress animation

---
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/jhoos/keymap.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+enum custom_layers {
+    _BL,
+    _FL,
+};
